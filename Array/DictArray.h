787aa6ff1c637b140f02641cd85131e5be0e763a
st006752 <114903428+st006752@users.noreply.github.com>
2026-05-12 10:31:45 +0300

    Removed interface inheritance in DictArray

--- Array/DictArray.h
+++ Array/DictArray.h
@@ -2,19 +2,18 @@
 #include <stdexcept>
 
 #include "Array.h"
-#include "ABC.h"
 
 
 template < typename key, typename value >
-class DictArray: public ABCDict<key, value> {
+class DictArray{
 public:
 	DictArray() = default;
 
 	// Обращение по ключу
-	value operator[](const key&) override;
+	value operator[](const key&);
 
 	// Добавление сопоставления
-	void insert(const key&, const value&) override;
+	void insert(const key&, const value&);
 
 private:
 	Array< DictPara<key,value> > data_;
